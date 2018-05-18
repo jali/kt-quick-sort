f192be305077b0c51f6f893dd3ef110ef7a1c138
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2018-05-18 16:05:43 +0100

    add partition recursive function

--- src/kotlin/main/QuickSort/QuickSort.kt
+++ src/kotlin/main/QuickSort/QuickSort.kt
@@ -1,2 +1,22 @@
-package kotlin.main.QuickSort
+package QuickSort
 
+typealias PairOfLists = Pair<List<Int>, List<Int>>
+
+class QuickSort {
+
+    fun partition(list: List<Int>, pivot: Int): PairOfLists {
+        fun loop(list: List<Int>, pivot:Int, result: PairOfLists): PairOfLists = when {
+            list.isEmpty() -> result
+            else ->
+                    if(list.first() < pivot) {
+                        loop(
+                                list.drop(1),
+                                pivot,
+                                PairOfLists(listOf(list.first()) + result.first, result.second))
+                    } else {
+                        loop(list.drop(1), pivot, PairOfLists(result.first, listOf(list.first()) + result.second))
+                    }
+        }
+        return loop(list, pivot, PairOfLists(listOf(), listOf()))
+    }
+}
