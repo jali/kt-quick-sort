53054883dce1c6b0f0d368d2708f1d2a937bc834
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2018-05-19 11:40:57 +0100

    refactor package name

--- src/kotlin/test/quicksort/QuickSortTest.kt
+++ src/kotlin/test/quicksort/QuickSortTest.kt
@@ -1,4 +1,4 @@
-package QuickSort
+package quicksort
 
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -9,7 +9,7 @@ class QuickSortTest {
     @Test
     fun partitionShouldReturnPairOfListsOfIntegerTest() {
         val list: List<Int> = listOf(23,53,11,54,12,55,19,32,56,99)
-        val pivot: Int = 12
+        val pivot = 12
         val quickSort = QuickSort()
         val actual = quickSort.partition(list, pivot)
         assertTrue(actual is Pair<List<Int>, List<Int>>, "Expected return pair of lists of integer")
